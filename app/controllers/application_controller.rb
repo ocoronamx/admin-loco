44ab393a6cfe921c22162f06691011c816a15836
ocoronamx <ocorona.desarrollo@gmail.com>
2024-05-03 21:55:09 -0600

    Improve home to do list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   def active_link?(controller, action)
     request.parameters['action'].eql?(action.to_s) && request.parameters['controller'].eql?(controller.to_s)
   end
+  def init_breadcrumb(title)
+    @breadcrumb = { title: title, list: [] }
+    @breadcrumb[:list] << { href: home_path, text: I18n.t(:'home.title') }
+  end
+  def add_breadcrumb(path, i18n)
+    @breadcrumb[:list] << { href: path, text: I18n.t(:"#{i18n}") }
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,35 @@
 class HomeController < ApplicationController
   def index
-    @breadcrumb = { title: 'Starter Page',
-      list: [
-        { href: home_path, text: I18n.t(:'home.title') }
-      ]
-    }
+    init_breadcrumb 'Starter Page'
+    init_todo
+
+    add_todo 0, 'Implementar Vue.js'
+    add_todo 0, 'Vista notices'
+
+    add_pending 0, 'Vista show'
+    add_pending 0, 'Implementar colorize'
+
+    set_done
+  end
+  def set_done
+    add_done 'Mejorar Home'
+    add_done  'Vista demo landing'
+    add_done  'Vista demo login'
+    add_done  'Vista demo index'
+    add_done  'Vista demo form'
+    add_done  'Migrar demos a su propio controlador'
+    add_done  'Implementar template adminlte'
+  end
+  def init_todo
+    @todos = { todo: [], pending: [], done: [] }
+  end
+  def add_todo(progress, task)
+    @todos[:todo] << { task: task, progress: progress }
+  end
+  def add_pending(progress, task)
+    @todos[:pending] << { task: task, progress: progress }
+  end
+  def add_done(task)
+    @todos[:done] << { task: task, progress: 100 }
   end
 end
